49f2f10b8f2e059aa3e72e78de081e989d304f85
Jose Angel Suarez <angelsuarez2612@gmail.com>
2024-03-17 19:33:56 -0300

    modified:   src/services/Tienda.Service.js

--- src/services/Tienda.Service.js
+++ src/services/Tienda.Service.js
@@ -3,42 +3,42 @@ import { Api_tienda } from "../constants/Api.tienda";
 
 class TiendaVirtualService {
 
-async getAllProducts() {
+  async getAllProducts() {
 
-    const response= await fetch(Api_tienda.Products());
+    const response = await fetch(Api_tienda.Products());
     return response.json();
-    
-  //  fetch('https://fakestoreapi.com/products')
-     //       .then(res=>res.json())
-         //   .then(json=>console.log(json))
-}
 
-async getAllProductsById(id) {
+    //  fetch('https://fakestoreapi.com/products')
+    //       .then(res=>res.json())
+    //   .then(json=>console.log(json))
+  }
 
-  const response = await fetch(Api_tienda.ProductsByID(id));
+  async getAllProductsById(id) {
+
+    const response = await fetch(Api_tienda.ProductsByID(id));
     return response.json();
-}
+  }
 
-async getAllProductsByCategory(){
-const response= await fetch(Api_tienda.ProductsByCategory());
-return response.json();
+  async getAllProductsByCategory() {
+    const response = await fetch(Api_tienda.ProductsByCategory());
+    return response.json();
 
-}
+  }
 
-async getAllProductsByTypeCategory (category) {
+  async getAllProductsByTypeCategory(category) {
 
-  const response= await fetch(Api_tienda.ProductsByTypeCategory(category))
-  return response.json();
+    const response = await fetch(Api_tienda.ProductsByTypeCategory(category))
+    return response.json();
 
-}
+  }
 
-async getAllCarts () {
+  async getAllCarts() {
 
-  const response= await fetch(Api_tienda.Cart())
-  return response.json();
+    const response = await fetch(Api_tienda.Cart())
+    return response.json();
 
-  
-}
+
+  }
 
 }
 
